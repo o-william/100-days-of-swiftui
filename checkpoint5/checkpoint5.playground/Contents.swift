908aafac51aabbdf247f24b97c9a59bc3226eca8
= <=>
2023-08-28 11:37:31 +0200

    Added comments that specified the OS version I was used, as well as XCode version.

--- checkpoint5/checkpoint5.playground/Contents.swift
+++ checkpoint5/checkpoint5.playground/Contents.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 // Author: Oluwapelumi Williams
-//
+// XCode: 14.3.1
+// MacOS: 13.5
+// 100 Days of Swift with Paul Hudson
+// Checkpoint 5
 
 /*
  You’ve already met sorted(), filter(), map(), so I’d like you to put them together in a chain – call one, then the other, then the other back to back without using temporary variables.
